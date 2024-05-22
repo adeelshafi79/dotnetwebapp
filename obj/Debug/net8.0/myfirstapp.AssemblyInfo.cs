ea840696e7f6ac9025a0f6f9b2ca4b81e1594af3
madnan08 <adnan.sarfraz.2302105@gmail.com>
2024-05-22 14:51:16 +0000

    port updated

--- obj/Debug/net8.0/myfirstapp.AssemblyInfo.cs
+++ obj/Debug/net8.0/myfirstapp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("myfirstapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d228a716594b6aeddb7cfbbbdf037da35079a9ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("myfirstapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("myfirstapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
